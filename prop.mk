4733b15751b03af2c94801453c1b2ca22eefa015
Jean-Michel Trivi <jmtrivi@google.com>
2017-06-22 19:20:26 -0700

    msm8953-common: Media volume: from 15 to 25 steps
    Test: play media, use all 25 vol steps and observe varying attenuation
    Bug: 32195657
    Change-Id: I7ac87df0b2b15de3a7bb87e739bd88ef7b185236
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- prop.mk
+++ prop.mk
@@ -19,6 +19,7 @@ persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=true \
 persist.vendor.btstack.enable.splita2dp=false \
 ro.af.client_heap_size_kbyte=7168 \
+ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=7 \
 ro.vendor.audio.sdk.fluencetype=fluence \
 ro.vendor.audio.sdk.ssr=false \
