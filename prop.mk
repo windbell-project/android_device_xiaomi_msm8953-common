d0aac56ee8b521691ce14437f61d999d174a6873
Daniel Sikorski <dadi.sikorski@gmail.com>
2017-08-15 00:00:44 +0200

    msm8953-common: dex2oat: use 8 cores instead of 4
    use more cores for faster app installation
    
    Change-Id: I7045673a28eed043ec45013b1abc48e5f3a5f7ba
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- prop.mk
+++ prop.mk
@@ -133,7 +133,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.fw.dex2oat_thread_count=4 \
+ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so
 
 # Netmgrd
