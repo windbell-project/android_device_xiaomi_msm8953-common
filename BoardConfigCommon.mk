d0f83f0ef498f0e8fd7fcc1f2b657d2a72c4a244
Shubham Singh <coolsks94@gmail.com>
2018-12-05 00:52:21 +0530

    msm8953-common: Update overlays
     * Nuke overlay-lineage
     * Nuke lineagehw
     * Nuke deps
     * Move required overlays to overlay
     * Add button backlight related overlays
    
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,9 +165,6 @@ TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
-# Lineage Hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(VENDOR_PATH)/lineagehw|**/*.java
-
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
