f9eac92c50a7fc10df2e9380a6d2fddc68274c7e
孔维楷 <2212848813@qq.com>
2019-04-05 20:36:42 +0800

    msm8953-common: Initial Changes for caf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,7 +197,6 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 endif
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_VERS := 28.0
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
