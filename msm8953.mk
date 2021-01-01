825e5b537f6b3d182b7ecd5a326765d780b70202
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-01 21:29:20 +0000

    msm8953-common: Drop soundtrigger hal

--- msm8953.mk
+++ msm8953.mk
@@ -80,7 +80,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
