db9a603669728be2b4c661f2ae207372f14cc48e
abhinavgupta371 <abhinavgupta371@gmail.com>
2021-03-10 17:41:58 +0000

    msm8953-common: Drop overlay-lineage
     Add AOSP overlays as well

--- msm8953.mk
+++ msm8953.mk
@@ -6,7 +6,6 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
