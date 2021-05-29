893d2405ce98046dd3219377ec836e209f9f2e0b
abhinavgupta371 <abhinavgupta371@gmail.com>
2021-05-29 11:20:25 +0000

    msm8953-common: Update Graphics & postprocessing from nokia
    Change-Id: Iaeeef05fdd6ddec344b2db2e384b78542bc08090

--- msm8953.mk
+++ msm8953.mk
@@ -316,10 +316,6 @@ PRODUCT_PACKAGES += \
     move_time_data.sh \
     move_wifi_data.sh
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
