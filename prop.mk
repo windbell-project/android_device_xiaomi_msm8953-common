931842f200571587840d236805e55cb6b34a04f8
kdrag0n <danny@kdrag0n.dev>
2021-06-18 18:19:26 +0530

    msm8953-common:Enable IORap app launch prefetching
    Commit b73d5516136d1a4de39afd74f564daf70b946889 in build/make (rvc-qpr2) disabled IORap by default due to a "regression" without mentioning what the regression is. Considering how it's been working fine for the past 6 months and has demonstrated improvements to app launch time [1], let's# Please enter the commit message for your changes. Lines starting re-enable it here.
    
    Signed-off-by: neonmicky <neonmicky30@gmail.com>

--- prop.mk
+++ prop.mk
@@ -116,6 +116,9 @@ persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
 
+# IORap
+ro.iorapd.enable=true
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
