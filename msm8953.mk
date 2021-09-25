98524305b34a2cc014e1207425619190ed2a3b14
mikanxx <1376106928@qq.com>
2021-09-25 11:56:58 +0800

    msm8953-common: Bring back Renderscript HAL

--- msm8953.mk
+++ msm8953.mk
@@ -322,6 +322,10 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.post_boot.sh
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
