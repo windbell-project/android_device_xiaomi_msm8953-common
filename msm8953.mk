5c3486d4187c77d8bb81b0f675416a94ef7b26e8
Tim Zimmermann <tim@linux4.de>
2022-06-13 09:19:40 +0530

    msm8953-common: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used

--- msm8953.mk
+++ msm8953.mk
@@ -157,6 +157,9 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
